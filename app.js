a168d12a0957fbfaa4825ac4c8f3ecc5da1f1377
MELTEM <meltemfsanaldi@gmail.com>
2022-09-18 00:18:56 +0300

    cl

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-// const ac = document.querySelector(".ac");
-// const ae = document.querySelector(".ae");
-// const yüzde = document.querySelector(".yüzde");
-// const böl = document.querySelector(".bölü");
-// const seven = document.querySelector(".seven");
-// const six = document.querySelector(".six");
-// const five = document.querySelector(".five");
-// const four = document.querySelector(".four");
-// const three = document.querySelector(".three");
-// const two = document.querySelector(".two");
-// const one = document.querySelector(".one");
-// const eight = document.querySelector(".eight");
-// const nine = document.querySelector(".nine");
-// const sub = document.querySelector(".sub");
-// const zero = document.querySelector(".zero");
-// const x = document.querySelector(".x");
-// const add = document.querySelector(".add");
-// const dot = document.querySelector(".dot");
-// const equal = document.querySelector(".equal");
-// const calc = document.querySelector(".calc");
-// const nums = document.querySelector(".nums")
-// const us = document.querySelector(".us")
-// const number = document.querySelector(".number")
-// let first = ""
-// let sec =""
-// let islem =""
-
-
-// nums.addEventListener("click", (e) => {
-// console.log(e.target.innerText);
-// if(e.target.classList.contains("number"))
-// calc.innerText += `${e.target.innerText}`
-// })
-
 let us = document.querySelector(".us")
 let secim = document.querySelector(".nums").children;
 let sonuc = document.querySelector(".calc");
@@ -53,7 +19,7 @@ function hesapla() {
   if (eval(sonuc.innerText.length) > 15) {
     // Çıkan sonuç 15 basamaktan büyükse 15 basamak olarak girdirildi.
 
-    sonuc.innerText = eval(sonuc.innerText).toPrecision(10);
+    sonuc.innerText = eval(sonuc.value).toPrecision(10);
   }
 }
 // "temizle" butonunun işlevi girildi.
@@ -75,3 +41,4 @@ secim.addEventListener("click",(e)=>{
     // sonuc.innerText = eval(sonuc.innerText);
    } 
 })
+
