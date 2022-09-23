684f5bab72651c9ee9b684a8b3167ef91926657c
MELTEM <meltemfsanaldi@gmail.com>
2022-09-23 12:26:28 +0300

    js

--- app.js
+++ app.js
@@ -1,44 +1,121 @@
-let us = document.querySelector(".us")
-let secim = document.querySelector(".nums").children;
-let sonuc = document.querySelector(".calc");
-let clean = document.querySelector(".ac");
-let hesap = document.querySelector(".equal");
-clean.addEventListener("click", temizle);
-hesap.addEventListener("click", hesapla);
-// Diğer tüm tuşlar için dinleyiciler tanımlandı.
-for (let i = 0; i < 20; i++) {
-  if (i != 0 && i != 18) {
-    // "temizle" ve "hesapla" butonları hariç tutuldu.
-    secim[i].addEventListener("click", yazdir);
-    
+const up = document.querySelector(".previous-display");
+const down = document.querySelector(".current-display");
+const buttons = document.querySelector(".buttons-container");
+
+let currOperand = "";
+let previousOperand = "";
+let operation = "";
+
+let equalOrPercentPressed = false;
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("num")) {
+    appendNumber(e.target.textContent);
+    updateDisplay();
   }
-}
-// "eval" ile hesaplama yapıldı.
-function hesapla() {
-  sonuc.innerText = eval(sonuc.innerText);
-  if (eval(sonuc.innerText.length) > 15) {
-    // Çıkan sonuç 15 basamaktan büyükse 15 basamak olarak girdirildi.
 
-    sonuc.innerText = eval(sonuc.value).toPrecision(10);
+  if (e.target.classList.contains("operator")) {
+    chooseOperator(e.target.textContent);
+    updateDisplay();
   }
-}
-// "temizle" butonunun işlevi girildi.
-function temizle() {
-  sonuc.innerText = " ";
-}
-// Tıklanan tuşlar sonuç ekranını atandı.
-function yazdir() {
-  if (sonuc.innerText.length < 15)
-    // 15 basamaktan fazla olması engellendi.
-    sonuc.innerText = sonuc.innerText + this.innerText;
-}
 
-secim.addEventListener("click",(e)=>{
-    console.log(e.target);
-   if(e.target.classList.contains("islem")){
-    us.innerText = sonuc.innerText;
-    console.log(sonuc.innerText);
-    // sonuc.innerText = eval(sonuc.innerText);
-   } 
-})
+  if (e.target.classList.contains("equal")) {
+    calculate();
+    updateDisplay();
+    equalOrPercentPressed = true;
+  }
 
+  if (e.target.classList.contains("ac")) {
+    previousOperand = "";
+    currOperand = "";
+    operation = "";
+    updateDisplay();
+  }
+
+  if (e.target.classList.contains("ae")) {
+    if (!currOperand) return;
+    currOperand *= -1;
+    updateDisplay();
+  }
+
+  if (e.target.classList.contains("percent")) {
+    if (!currOperand) return;
+    currOperand = currOperand /100
+    updateDisplay();
+    equalOrPercentPressed = true;
+  }
+});
+
+const appendNumber = (num) => {
+  if (currOperand === "0" && num === "0") return;
+
+  if (currOperand === "0" && num !== ".") {
+    currOperand = num;
+    return;
+  }
+
+  if (num === "." && currOperand.includes(".")) return;
+
+  if (currOperand.length > 10) return;
+
+  if (equalOrPercentPressed) {
+    currOperand = num;
+    equalOrPercentPressed = false;
+    return;
+  }
+  currOperand += num;
+};
+
+const updateDisplay = () => {
+  if (currOperand.toString().length > 11) {
+    currOperand = Number(currOperand).toExponential(3);
+  }
+  down.textContent = currOperand;
+
+  //! Eger bir sayiya basilmadan operator butonlarina basilirsa
+  //! up'de operatoru gosterme
+  if (operation && previousOperand) {
+    up.textContent = `${previousOperand} ${operation}`;
+  } else {
+    up.textContent = "";
+  }
+};
+
+const chooseOperator = (op) => {
+  if (previousOperand) {
+    calculate();
+  }
+
+  operation = op;
+  previousOperand = currOperand;
+  currOperand = "";
+};
+
+const calculate = () => {
+  let calculation = 0;
+
+  const prev = Number(previousOperand);
+  const current = Number(currOperand);
+
+  switch (operation) {
+    case "+":
+      calculation = prev + current;
+      break;
+    case "-":
+      calculation = prev - current;
+      break;
+    case "x":
+      calculation = prev * current;
+      break;
+    case "÷":
+      calculation = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  currOperand = calculation;
+
+  previousOperand = "";
+  operation = "";
+};
